70fa1c0ebcdfbc7dc4c5769a38157d5c1d129735
yaoyao <31313123@qq.com>
2023-12-26 17:34:11 +0800

    first

--- src/config/config.go
+++ src/config/config.go
@@ -1,1 +1,18 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+func Init() {
+	viper.AddConfigPath("./")
+	viper.SetConfigFile(".env")
+	viper.SetConfigType("yaml")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	if err != nil {
+		panic(err)
+	}
+}
